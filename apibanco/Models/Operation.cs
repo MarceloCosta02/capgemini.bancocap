8d12f7e4adbb14962aa1929a8e0fccaf1b700fb0
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-27 18:10:12 -0300

    Criando as demais models

---
+++ apibanco/Models/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibanco.Models
+{
+    public class Operation
+    {
+        public string Hash { get; set; }
+        public char Type { get; set; }
+        public double Value { get; set; }
+        public DateTime DataHora { get; set; }
+        public int IdContaOrigem { get; set; }
+        public int IdContaDestino { get; set; }
+
+    }
+}
---
+++ apibanco/Models/Transfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibanco.Models
+{
+    public class Transfer
+    {
+        public string HashOrigin { get; set; }
+        public string HashDestiny { get; set; }
+        public double Value { get; set; }
+
+    }
+}
