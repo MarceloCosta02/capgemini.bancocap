845fb802f782222d15d87e6da8f0c2de98aad0bf
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-27 17:27:16 -0300

    Implementacao do repository e do service de account

---
+++ apibanco/Repository/AccountRepository.cs
@@ -0,0 +1,53 @@
+﻿using apibanco.Interfaces.Repository;
+using apibanco.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibanco.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly string _connectionString;
+
+        public AccountRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        /// <summary>
+        /// Metodo para inclusao de Contas no banco de dados
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public void Insert(Account account)
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            var query = "insert into conta (Hash, IdCliente) " +
+                            "values (@Hash, @IdCliente)";
+
+            var result = connection.Execute(query, new
+            {
+                Hash = account.Hash,
+                IdCliente = account.IdCliente
+            });
+        }
+
+        public int VerifyIfHashExists(string hash)
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            var query = "select count(Hash) from conta " +                           
+                            " where Hash = @Hash";
+
+            var result = connection.Query<int>(query, new { Hash = hash });
+
+            return result.FirstOrDefault();
+        }
+    }
+}
---
+++ apibanco/Services/AccountService.cs
@@ -0,0 +1,64 @@
+﻿using apibanco.Interfaces.Repository;
+using apibanco.Interfaces.Service;
+using apibanco.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibanco.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repo;
+
+        public AccountService(IAccountRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void InsertAccount(Account account)
+        {
+            bool validateHash = false;
+            string hash = "";
+
+            while (validateHash == false)
+            {
+                hash = GenerateHash();
+                validateHash = VerifyIfHashExists(hash);
+            }
+
+            account.Hash = hash;
+            _repo.Insert(account);
+        }
+
+        /// <summary>
+        /// Metodo que verifica se o hash existe já no banco
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public bool VerifyIfHashExists(string hash)
+        {
+            var exists = _repo.VerifyIfHashExists(hash);
+            if (exists == 0)
+                return true;
+            else
+                return false;
+        }
+
+        private string GenerateHash()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUWXYZ123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 8)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            return result;
+        }
+
+    }
+}
